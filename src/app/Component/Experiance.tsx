c1ac8c3ce388afe3d07a364c787ece9897098d10
tanvirh103 <tanvirh103@gmail.com>
2025-03-28 14:52:07 +0600

    updated

--- src/app/Component/Experiance.tsx
+++ src/app/Component/Experiance.tsx
@@ -1,29 +1,40 @@
 export default function Experience() {
   return (
-    <div className="mt-6 mb-6 max-w-1400px lg:max-w-[1400px] 2xl:max-w-[1560px] md:max-w-[1200px] sm:max-w-[1020px] mx-auto px-4 2xl:px-12 lg:px-8">
+    <div className="pt-6 pb-6 max-w-1400px lg:max-w-[1400px] 2xl:max-w-[1560px] md:max-w-[1200px] sm:max-w-[1020px] mx-auto px-4 2xl:px-12 lg:px-8">
       <div className="items-center text-center pt-4">
         <p className="text-[40px] font-[600] text-[#8dced0]">Experiance</p>
         <p className="text-[24px] font-[600] ">
           Combining hand - on industry experiance and volunteer experience
         </p>
       </div>
+      <div>
+        <p className="font-[600] text-[24px]">Work Experience</p>
+      </div>
       <div className="pt-6 pb-6 w-full">
         <div
           className="bg-[#81baff] rounded-full w-5 h-5"
           style={{ zIndex: 115 }}
         ></div>
-        <div className="border-l-2 border-l-inherit h-24 mx-2">
-          <div className="-mt-[22px]">
-            <p className="font-[500] text-[20px] ml-4 ">DeepChainLabs</p>
-            <p>
-              Jr Software Engineer September 2024 - March 2025 (Remote) -
-              Developed and delivered full-stack solutions for 'Kazentic,'
-              enhancing user experience and system performance. - Engineered
-              dynamic front-end features for 'Trading Bot,' significantly
-              improving responsiveness and usability.
+        <div className="border-l-2 border-l-inherit h-36 mx-2">
+          <div className="-mt-[22px] w-[600px]">
+            <p className="font-[500] text-[20px] ml-4 ">Deep Chain Labs</p>
+            <p className="font-[500] text-[16px] ml-4">
+              Software Engineer Trainee October 2024 - February 2025 (Remote)
+            </p>
+            <p className="font-[500] text-[16px] ml-4">
+              {" "}
+              - Developed and delivered full-stack solutions for
+              'Kazentic',enhancing user experience and system performance.
+            </p>
+            <p className="font-[500] text-[16px] ml-4">
+              - Engineered dynamic front-end features for 'Trading Bot,'
+              significantly improving responsiveness and usability.
             </p>
           </div>
         </div>
+      </div>
+      <div>
+        <p className="font-[600] text-[24px]">Volunteer Experience</p>
       </div>
     </div>
   );
--- src/app/Component/Navbar.tsx
+++ src/app/Component/Navbar.tsx
@@ -1,16 +1,22 @@
-'use client'
-import { Facebook, Github, LinkendIn, Mail, MobileMenuIcon } from "@/SVG/WelcomeSvg";
-import Link from "next/link"
+"use client";
+import {
+  Facebook,
+  Github,
+  LinkendIn,
+  Mail,
+  MobileMenuIcon,
+} from "@/SVG/WelcomeSvg";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import MobileMenu from "./MobileMenu";
 
 export default function Navbar() {
   const [isShowSideMenu, setIsShowSideMenu] = useState(false);
-  const [isShowMenu,setShowMenu]=useState(false)
+  const [isShowMenu, setShowMenu] = useState(false);
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const handleResize = () => {
@@ -21,57 +27,68 @@ export default function Navbar() {
     }
   };
 
-
   return (
-    <div>    <div className="sticky top-0 w-full z-50 max-w-1400px lg:max-w-[1400px] 2xl:max-w-[1560px] md:max-w-[1200px] sm:max-w-[1020px] mx-auto px-4 2xl:px-12 lg:px-8 pt-8 bg-[#000000]">
-      <div className="flex justify-between items-center">
-        <div className="flex justify-start">
-          <p className="font-[600] text-[24px]">Tanvir Hasan Tamal</p>
-        </div>
-        {!isShowSideMenu?(
-          <>
-          <div className="flex justify-center gap-12">
-        <p className="font-[500] text-[16px] hover:text-[#81baff]">Home</p>
-        <p className="font-[500] text-[16px] hover:text-[#81baff]">About</p>
-        <p className="font-[500] text-[16px] hover:text-[#81baff]">Projects</p>
-        <p className="font-[500] text-[16px] hover:text-[#81baff]">Experiance</p>
-        <p className="font-[500] text-[16px] hover:text-[#81baff]">Contact</p>
-        </div>
-        <div className="flex justify-end gap-6">
-        <Link href={`https://github.com/tanvirh103`}>
-          <p>
-            <Github/>
-          </p>
-          </Link>
-          <Link href={`https://www.linkedin.com/in/tanvirh103/`}>
-          <p>
-           <LinkendIn/>
-          </p>
-          </Link>
-          <Link href={`mailto:tanvirh103@gmail.com`}>
-          <p>
-            <Mail/>
-          </p>
-          </Link>
-          <Link href={`https://www.facebook.com/tanvirh103/`}>
-          <p>
-           <Facebook/>
-          </p>
-          </Link>
-        </div>
-          </>
-        ):(
-          <div onClick={()=>{setShowMenu(true)}}>
-          <MobileMenuIcon/>
+   
+   
+      <div className="sticky top-0 w-full z-50 max-w-1400px lg:max-w-[1400px] 2xl:max-w-[1560px] md:max-w-[1200px] sm:max-w-[1020px] mx-auto px-4 2xl:px-12 lg:px-8 pt-6 pb-4 bg-[#000000]">
+        <div className="flex justify-between items-center">
+          <div className="flex justify-start">
+            <p className="font-[600] text-[24px]">Tanvir Hasan Tamal</p>
           </div>
-        )}
+          {!isShowSideMenu ? (
+            <>
+              <div className="flex justify-center gap-12">
+                <p className="font-[500] text-[16px] hover:text-[#81baff]">
+                  Home
+                </p>
+                <p className="font-[500] text-[16px] hover:text-[#81baff]">
+                  About
+                </p>
+                <p className="font-[500] text-[16px] hover:text-[#81baff]">
+                  Projects
+                </p>
+                <p className="font-[500] text-[16px] hover:text-[#81baff]">
+                  Experiance
+                </p>
+                <p className="font-[500] text-[16px] hover:text-[#81baff]">
+                  Contact
+                </p>
+              </div>
+              <div className="flex justify-end gap-6">
+                <Link href={`https://github.com/tanvirh103`}>
+                  <p>
+                    <Github />
+                  </p>
+                </Link>
+                <Link href={`https://www.linkedin.com/in/tanvirh103/`}>
+                  <p>
+                    <LinkendIn />
+                  </p>
+                </Link>
+                <Link href={`mailto:tanvirh103@gmail.com`}>
+                  <p>
+                    <Mail />
+                  </p>
+                </Link>
+                <Link href={`https://www.facebook.com/tanvirh103/`}>
+                  <p>
+                    <Facebook />
+                  </p>
+                </Link>
+              </div>
+            </>
+          ) : (
+            <div
+              onClick={() => {
+                setShowMenu(true);
+              }}
+            >
+              <MobileMenuIcon />
+            </div>
+          )}
+        </div>
+        {isShowMenu && <MobileMenu setShow={setShowMenu} />}
       </div>
-     
-    </div>
-    {isShowMenu&&(
-        <MobileMenu setShow={setShowMenu}/>
-      )}
-    </div>
-
+    
   );
 }
