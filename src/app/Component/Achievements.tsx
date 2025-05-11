f4b048db7ee355c6ccfdf6bef2c9aca833bd9873
tanvirh103 <tanvirh103@gmail.com>
2025-05-11 14:47:37 +0600

    padding added

--- src/app/Component/Achievements.tsx
+++ src/app/Component/Achievements.tsx
@@ -2,7 +2,7 @@ import { AchievementsContent } from "../Content/Achievements";
 
 export default function Achievements() {
   return (
-    <div className="mt-6 max-w-1400px lg:max-w-[1400px] 2xl:max-w-[1560px] md:max-w-[1200px] sm:max-w-[1020px] mx-auto px-4 2xl:px-12 lg:px-8">
+    <div className="mt-6 pt-6 pb-6 max-w-1400px lg:max-w-[1400px] 2xl:max-w-[1560px] md:max-w-[1200px] sm:max-w-[1020px] mx-auto px-4 2xl:px-12 lg:px-8">
       <div className="items-center text-center pt-4">
         <p className="text-[24px] md:text-[36px] lg:text-[40px] font-[600] text-[#8dced0]">
           Achievements
