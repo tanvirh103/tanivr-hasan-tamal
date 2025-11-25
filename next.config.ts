d24390f6c600f95799da51daa1bd6e8fb16fce3a
Tanvir Hasan Tamal <tanvirh103@gmail.com>
2025-11-25 23:17:47 +0600

    updated

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "tanvirh103.github.io",
-      pathname:"/tanvir-hasan-tamal/**"
-     }],
+    unoptimized:true,
   },
 };
 export default nextConfig;
