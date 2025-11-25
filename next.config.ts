f794efe2617c841b96f462bab250e0b46a2eb086
Tanvir Hasan Tamal <tanvirh103@gmail.com>
2025-11-25 23:25:06 +0600

    .

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
-    unoptimized:true,
+    unoptimized: true,
   },
 };
+
 export default nextConfig;
